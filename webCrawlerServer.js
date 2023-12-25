8f05b0c2cf0a598f1e55cacca80aeeee97e33c65
Jason Kwon <jasonproject628@gmail.com>
2023-12-25 13:59:45 +0900

    Refactor : 나드리부름 포트 확인용 코드 수정
    test

--- webCrawlerServer.js
+++ webCrawlerServer.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const cors = require("cors");
 const app = express();
-const PORT = 81;
+const PORT = 3001;
 
 app.use(cors());
 app.use(express.json());
